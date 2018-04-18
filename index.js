9818444be1a66f6beb9159ac291cb538836f5730
Andres Cuervo <andrescuervor@gmail.com>
2018-04-18 03:53:45 -0700

    Add unsupported browser message

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ require('./three.ar.js')
 require('./aframe-ar-min.ar.js')
 import * as wasm from './wasm.rs';
 
+// Mostly from: https://github.com/google-ar/three.ar.js/blob/a36b9d1ba85a8a11e54622c6d0d29e7c028a636c/src/ARUtils.js
+// but with a couple style tweaks (to counter A-Frame + make it transparent)
+const LEARN_MORE_LINK = 'https://developers.google.com/ar/develop/web/getting-started';
+const UNSUPPORTED_MESSAGE = `This augmented reality experience requires
+  WebARonARCore or WebARonARKit, experimental browsers from Google
+  for Android and iOS. Learn more at the <a href="${LEARN_MORE_LINK}">Google Developers site</a>.`;
+
+THREE.ARUtils.displayUnsupportedMessage = customMessage => {
+  const element = document.createElement('div');
+  element.id = 'webgl-error-message';
+  element.style.fontFamily = 'monospace';
+  element.style.fontSize = '13px';
+  element.style.fontWeight = 'normal';
+  element.style.textAlign = 'center';
+  element.style.background = 'rgba(255, 255, 255, 0.7)';
+  element.style.border = '1px solid black';
+  element.style.color = '#000';
+  element.style.padding = '1.5em';
+  element.style.width = '400px';
+  element.style.position = 'absolute';
+  element.style.top = '0';
+  element.style.left = '0';
+  element.innerHTML = typeof(customMessage) === 'string' ? customMessage : UNSUPPORTED_MESSAGE;
+  document.body.appendChild(element);
+};
+
+THREE.ARUtils.getARDisplay().then(function (display) {
+  if (!display) {
+      console.log("unsupported?");
+      THREE.ARUtils.displayUnsupportedMessage()
+  }
+});
+
 
 AFRAME.registerComponent('animate-scale-wasm', {
     schema: {
